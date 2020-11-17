dc6b2c7224c24c38520e9d328e232ee287b91b98
fehiora <mafe.hior@gmail.com>
2020-11-17 11:37:00 -0500

    Front: Loggin ok

--- frontend/src/app/registro-usuario/registro-usuario.component.ts
+++ frontend/src/app/registro-usuario/registro-usuario.component.ts
@@ -31,7 +31,7 @@ export class RegistroUsuarioComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  registrar(){
+  registroUsuario(){
     this.auth.registroUsuario(this.registrarUsuario).subscribe(
       (res) => {
         console.log(res);
